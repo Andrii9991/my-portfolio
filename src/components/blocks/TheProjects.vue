a969e411b86ad196096401906a6305c98475c3ef
Andrii9991 <andriymyshchyshyn6@gmail.com>
2025-01-30 20:39:59 +0200

    fix

--- src/components/blocks/TheProjects.vue
+++ src/components/blocks/TheProjects.vue
@@ -1,20 +1,24 @@
 <template>
   <section
-    class="flex w-full justify-center from-[#8F98A4] to-[#DBEBFF] md:bg-gradient-to-tr lg:mt-[-120px] xl:rounded-t-[160px]"
+    class="flex w-full justify-center from-[#8F98A4] to-[#DBEBFF] pb-12 md:bg-gradient-to-tr lg:mt-[-120px] xl:rounded-t-[160px]"
     id="projects"
   >
     <!-- Wrapper -->
     <div
-      class="mt-6 grid w-full max-w-7xl grid-cols-[repeat(auto-fill,minmax(220px,80%))] justify-center gap-3 md:grid-cols-[repeat(auto-fill,minmax(330px,1fr))]"
+      class="mt-6 grid w-full max-w-7xl grid-cols-[repeat(auto-fill,minmax(200px,80%))] justify-center gap-3 md:grid-cols-[repeat(auto-fill,minmax(330px,1fr))]"
     >
       <!-- Container -->
       <div
         data-aos="fade-up"
         v-for="project in projectsData"
         :key="project.title"
-        class="mx-2 mt-6 flex flex-col items-center rounded-[30px] bg-lightGrey p-6"
+        class="mx-2 mt-6 flex flex-col items-center rounded-[30px] bg-lightGrey p-6 xs:p-4"
       >
-        <img class="mb-3" :src="project.image" alt="project-image" />
+        <img
+          class="mb-3 cursor-pointer duration-200 hover:scale-105"
+          :src="project.image"
+          alt="project-image"
+        />
 
         <h1 class="gradient-text mb-3 font-russoOne text-xl font-normal">
           {{ project.title }}
@@ -36,8 +40,27 @@
             </span>
           </div>
         </div>
-
-        <div class="flex justify-between">LOGO LOGO</div>
+        <!-- Link for site block -->
+        <div class="my-3 flex w-full justify-center">
+          <a href="goole.com">
+            <span class="text-[10px] font-medium leading-[28px]"
+              >Live Demo</span
+            >
+            <img
+              class="duration-200 hover:scale-105"
+              src="../../public/img/projects-image/link-image/livedemo.svg"
+              alt="link for site"
+            />
+          </a>
+          <a class="ml-4" href="goole.com">
+            <span class="text-[10px] font-medium leading-[28px]">Code</span>
+            <img
+              class="duration-200 hover:scale-105"
+              src="../../public/img/projects-image/link-image/github.svg"
+              alt="link for code"
+            />
+          </a>
+        </div>
       </div>
     </div>
   </section>
