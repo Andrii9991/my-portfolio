71df531604fa36f4bdb3156989cc71a00338aae6
Andrii9991 <andriymyshchyshyn6@gmail.com>
2025-02-01 11:21:07 +0200

    style(projects): add new style

--- src/components/general/PFooter.vue
+++ src/components/general/PFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer
     data-aos="fade-up"
-    class="flex h-[300px] flex-col items-center rounded-t-[30px] bg-lightGrey px-8 py-3 xs:px-10 xs:py-4 md:rounded-t-[50px] md:px-16"
+    class="flex h-[300px] flex-col items-center rounded-t-[30px] bg-lightGrey px-8 py-3 xs:px-10 xs:py-4 md:rounded-t-[50px] md:px-16 lg:mt-[-145px] xl:rounded-t-[160px]"
     id="contacts"
   >
     <h1
